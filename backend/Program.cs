fc470219c4cd9f835430d6b60de4d74980d7e4a9
Fuki Sato <lego.sato.4135@gmail.com>
2023-11-25 14:19:18 +0900

    add management tools

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,5 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DB>(provider => provider.UseSqlServer(builder.Configuration.GetConnectionString("DB")));
 
 var app = builder.Build();
 
