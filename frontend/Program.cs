e4db7d8ebbe27925faf860623a9211f160a09e78
Fuki Sato <lego.sato.4135@gmail.com>
2024-02-12 21:38:12 +0900

    add google auth

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using frontend;
 using Blazored.LocalStorage;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Google", options.ProviderOptions);
+});
 
 await builder.Build().RunAsync();
