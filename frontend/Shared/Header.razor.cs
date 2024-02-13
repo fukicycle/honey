0427e244448549239109923633dbfd48b011edaf
Fuki Sato <lego.sato.4135@gmail.com>
2024-02-13 22:29:17 +0900

    update header

--- frontend/Shared/Header.razor.cs
+++ frontend/Shared/Header.razor.cs
@@ -1,4 +1,8 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace frontend.Shared
 {
@@ -6,9 +10,22 @@ namespace frontend.Shared
     {
         [Inject]
         public NavigationManager NavigationManager { get; set; } = null!;
+        [Inject]
+        public SignOutSessionStateManager SignOutManager { get; set; } = null!;
         private void UserIconOnClick()
         {
-            NavigationManager.NavigateTo("login");
+            NavigationManager.NavigateTo("authentication/login");
+        }
+
+        private async Task BeginSignOut(MouseEventArgs args)
+        {
+            await SignOutManager.SetSignOutState();
+            NavigationManager.NavigateTo("",true);
+        }
+
+        private string GetProfilePicture(AuthenticationState context)
+        {
+            return context.User.Claims.FirstOrDefault(a => a.Type == "picture")?.Value ?? "";
         }
     }
 }
